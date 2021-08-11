d4320d476b8ad75f8611dde898ee1f7b92146caf
Воронцов Роман <r.vorontsov+GitHub@yandex.ru>
2021-08-11 18:13:20 +0300

    Added models

---
+++ EquatableSourceGenerator.Sample/Models/AnotherDummyModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EquatableSourceGenerator.Sample.Models
+{
+    public partial class AnotherDummyModel : IEquatable<AnotherDummyModel>
+    {
+        public int Id { get; init; }
+        public DateTime? CreationDate { get; init; }
+    }
+}
---
+++ EquatableSourceGenerator.Sample/Models/DummyModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EquatableSourceGenerator.Sample.Models
+{
+    public partial class DummyModel : IEquatable<DummyModel?>
+    {
+        public int Id { get; init; }
+        public bool IsActive { get; init; }
+        public string? DummyName { get; init; }
+        public AnotherDummyModel? Model { get; init; }
+        
+    }
+}
